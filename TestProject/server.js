686a98e8ad29a64c9dc5839a5dc338c0a5d48c09
MIKE MARAMBIO <u1229804@umail.utah.edu>
2020-10-15 21:29:56 -0600

    testing the Console App feature

--- TestProject/server.js
+++ TestProject/server.js
@@ -3,8 +3,8 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
- 
+    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
+    console.log()
     res.end('Hello World\n');
     
 }).listen(port);
