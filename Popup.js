73801cc004a6d239c942eb8798dbf373e140368c
MIKE MARAMBIO <u1229804@umail.utah.edu>
2020-10-17 18:50:04 -0600

    working add list but remove is not removing

--- Popup.js
+++ Popup.js
@@ -11,10 +11,11 @@
 
 // This is the js add / remove
 
-document.getElementById("btn btn-add").addEventListener('click', addItem);
-document.getElementById("btn btn-remove").addEventListener('click', removeItem);
+//document.getElementById("btn btn-add").addEventListener('click', addItem);
+//document.getElementById("btn btn-remove").addEventListener('click', removeItem);
 
-
+document.getElementById("btn btn-white-add").addEventListener('click', whiteListAddItem);
+document.getElementById("btn btn-white-remove").addEventListener('click', whiteListRemoveItem);
 
 function addItem() {
 
@@ -33,4 +34,24 @@ function addItem() {
      var candidate = document.getElementById("candidate");
     var item = document.getElementById(candidate.value);
      ul.removeChild(item);
+ }
+
+ function whiteListAddItem()
+ {
+
+    var ul = document.getElementById("dynamicWhiteList");
+    var candidate = document.getElementById("whiteListCandidate");
+    var li = document.createElement("li");
+    li.setAttribute('id', candidate);
+    li.appendChild(document.createTextNode(candidate.value));
+    ul.appendChild(li);
+ }
+ 
+ function whiteListRemoveItem()
+ {
+    var ul = document.getElementById("dynamicWhiteList");
+    var candidate = document.getElementById("whiteListCandidate");
+    var item = document.getElementById(candidate.value);
+    ul.removeChild(item);
+
  }
