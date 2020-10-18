7b679100511c79fe13f2459cb9b6fcd02afff6ca
MIKE MARAMBIO <u1229804@umail.utah.edu>
2020-10-17 18:12:58 -0600

    Added test user add-in pushing results
    Co-authored-by: u1203182@umail.utah.edu <u1203182@umail.utah.edu>

--- Popup.js
+++ Popup.js
@@ -1,11 +1,63 @@
-$(document).ready(function()
-{
-	$(document).on('click','.list-down-btn',function(event)
-  {
-  	event.preventDefault();
-    var target = $(this).attr('data-toggle');
-    $(target).slideToggle();
-    var clicked = event.target;
-    $(clicked).toggleClass("glyphicon-chevron-down  glyphicon-chevron-up");
-  });
-});
+
+
+
+
+//     <div class="container">
+//     <div class="row">
+//       <div class="col-12">
+
+
+//         <h1>Dynamically Add & Remove Items From List Using JavaScript</h1>
+//         <hr>
+
+
+//         <div class="input-group">
+//           <input type="text" class="form-control" id="candidate" required>
+//           <div class="input-group-append">
+//             <button  id="btn btn-add" type="button">Add Item</button>
+//             <button  id="btn btn-remove" type="button">Remove Item</button>
+//           </div>
+//         </div>
+
+// <!-- ------------------------------------ -->
+//         <ul id="dynamic-list">
+
+
+//         </ul>
+
+//       </div>
+//     </div>
+//   </div>
+
+
+
+
+
+
+
+
+// This is the js add / remove
+
+document.getElementById("btn btn-add").addEventListener('click', addItem);
+document.getElementById("btn btn-remove").addEventListener('click', removeItem);
+
+
+
+function addItem() {
+
+
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("candidate");
+    var li = document.createElement("li");
+    li.setAttribute('id', candidate.value);
+    li.appendChild(document.createTextNode(candidate.value));
+    ul.appendChild(li);
+}
+
+
+ function removeItem() {
+     var ul = document.getElementById("dynamic-list");
+     var candidate = document.getElementById("candidate");
+    var item = document.getElementById(candidate.value);
+     ul.removeChild(item);
+ }
