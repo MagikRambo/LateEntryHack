16002be8dd73b2dd50104df5e8f9cbf194c3a950
Jordan Jimenez <jordanjimenez510@gmail.com>
2020-10-18 15:29:53 -0600

    Commented Timer Code

--- testTimer.js
+++ testTimer.js
@@ -1,115 +1,58 @@
-
-// document.write('<script src="TimerModel.js" ></script>')
-
-// document.getElementById('timerButton').addEventListener('click', updateTimer);
-
-// //var startingMinutes;
-// //var time;
-// //var countdownEl;
-
-// var hours;
-// var minutes;
-// var seconds;
-// var input;
-// var interval;
-
-// function timeButtonClick()
-// {
-//      //startingMinutes = document.getElementById('countdown').value ; // change this to time inputted
-//      //time = startingMinutes * 60;
-//      if(getTimeRemainingHours() == 0 && getTimeRemainingMinutes() == 0 && getTimeRemainingSeconds() == 0)
-//         clearInterval(interval);
-//      countdownEl.value =  getTimeRemainingHours() + " :" + getTimeRemainingMinutes() + " :" + getTimeRemainingSeconds();
-     
-// }
-
-// function updateTimer()
-// {
-//     input = document.getElementById('countdown').value;
-//      countdownEl = document.getElementById('countdown');
-//      input = input.split(":");
-//      hours = input[0];
-//      minutes = input[1];
-//      seconds = input[2];
-     
-//     // Initialize timer
-//     initializeTimer();
-
-//      // Set Time Remaining
-//      setTimeRemaining(hours, minutes, seconds);
-
-//      // Start timer
-//      startTimer();
-     
-//     interval = setInterval(timeButtonClick, 50);
-    
-// }
-
-
-
-
-
 document.write('<script src="TimerModel.js" ></script>')
 
+// Grab Submit and GetTime Buttons
 document.getElementById('timerButton').addEventListener('click', updateTimer);
-//var startingMinutes;
-//var time;
-var countdownEl;
+document.getElementById('getTimeButton').addEventListener('click', updateTimer);
 
-var hours;
-var minutes;
-var seconds;
-var input;
-var interval;
+var hours, minutes, seconds, input, interval, countdownE1;
 
-function timeButtonClick()
-{
-     //startingMinutes = document.getElementById('countdown').value ; // change this to time inputted
-     //time = startingMinutes * 60;
-     if(getTimeRemainingHours() == 0 && getTimeRemainingMinutes() == 0 && getTimeRemainingSeconds() == 0)
+/**
+ * Display the time to the form. Clear Interval if time goes below 0.
+ */
+function displayTime()
+{  
+    if(getTimeRemainingHours() == 0 && getTimeRemainingMinutes() == 0 && getTimeRemainingSeconds() == 0)
         clearInterval(interval);
-     countdownEl.value =  getTimeRemainingHours() + " :" + getTimeRemainingMinutes() + " :" + getTimeRemainingSeconds();
-     
+    countdownEl.value =  getTimeRemainingHours() + " :" + getTimeRemainingMinutes() + " :" + getTimeRemainingSeconds(); 
 }
 
+/**
+ * Set the time correctly using methods from TimerModel.js
+ */
 function updateTimer()
 {
+    // Check if the time has been modified
     var hadRun = JSON.parse(localStorage.getItem("isRunningLocal"));
-    console.log(hadRun);
+
     if(!hadRun)
     {
-    input = document.getElementById('countdown').value;
-     countdownEl = document.getElementById('countdown');
-     input = input.split(":");
-     hours = input[0];
-     minutes = input[1];
-     seconds = input[2];
+        // Set all the variables with the time the user input
+        input = document.getElementById('countdown').value;
+        countdownEl = document.getElementById('countdown');
+        input = input.split(":");
+        hours = input[0];
+        minutes = input[1];
+        seconds = input[2];
      
-    // Initialize timer
-    initializeTimer();
-
-     // Set Time Remaining
-     setTimeRemaining(hours, minutes, seconds);
-
-     // Start timer
-     startTimer();
+        // Initialize and start timer
+        initializeTimer();
+        setTimeRemaining(hours, minutes, seconds);
+        startTimer();
      
-    interval = setInterval(timeButtonClick, 50);
+        // Update time
+        interval = setInterval(displayTime, 50);
     }
     else
     {
+        // Set all variables with timer that has already started
         countdownEl = document.getElementById('countdown');
         hours = getTimeRemainingHours();
         minutes = getTimeRemainingMinutes();
         seconds = getTimeRemainingSeconds();
 
+        // Continue time
         setTimeRemaining(hours, minutes, seconds);
-
         startTimer();
-        console.log(hours);
-        interval = setInterval(timeButtonClick, 50);
+        interval = setInterval(displayTime, 50);
     }
-
-    
-    
 }
